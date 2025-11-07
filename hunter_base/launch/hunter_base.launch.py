6e3d3f43603c975ed89fc4b7439de13f2ce588ba
nevlife <pcdpcd100@gmail.com>
2025-11-07 18:36:31 +0900

    update

--- hunter_base/launch/hunter_base.launch.py
+++ hunter_base/launch/hunter_base.launch.py
@@ -17,7 +17,7 @@ def generate_launch_description():
                                          description='CAN bus name, e.g. can0')
     odom_frame_arg = DeclareLaunchArgument('odom_frame', default_value='odom_bae',
                                            description='Odometry frame id')
-    base_link_frame_arg = DeclareLaunchArgument('base_frame', default_value='base_link_bae',
+    base_link_frame_arg = DeclareLaunchArgument('base_frame', default_value='base_link',
                                                 description='Base link frame id')
     odom_topic_arg = DeclareLaunchArgument('odom_topic_name', default_value='odom_bae',
                                            description='Odometry topic name')
