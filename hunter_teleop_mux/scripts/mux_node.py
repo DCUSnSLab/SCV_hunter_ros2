f72ef1c1d983770d07a9b12a21edb0463bf43dca
nevlife <pcdpcd100@gmail.com>
2026-05-18 00:56:16 +0900

    update

--- hunter_teleop_mux/scripts/mux_node.py
+++ hunter_teleop_mux/scripts/mux_node.py
@@ -4,7 +4,7 @@ from rclpy.node import Node
 from rclpy.parameter import Parameter
 from rcl_interfaces.msg import SetParametersResult
 from geometry_msgs.msg import Twist
-from teleop_bot_msgs.msg import EStopStatus, CmdMode, MuxStatus
+from teleop_rover_msgs.msg import EStopStatus, CmdMode, MuxStatus
 import time
 from typing import cast
 
